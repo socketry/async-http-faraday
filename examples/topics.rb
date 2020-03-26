a1ab5bb871b45b60ac2ad5a928cac22932e5794c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-03-27 02:57:56 +1300

    Add topics example.

---
+++ examples/topics.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require 'async'
+require 'faraday'
+require 'async/http/faraday'
+
+# Async.logger.debug!
+
+module TestAsync
+	URL = 'https://www.google.com/search'
+	TOPICS = %W{ruby python lisp javascript cobol}
+	
+	def self.fetch_topics_async
+		TOPICS.map do |topic|
+			Async do
+				Faraday.get("#{URL}?q=#{topic}")
+			end
+		end.map(&:wait)
+	end
+end
+
+Faraday.default_adapter = :async_http
+
+Async do
+	pp TestAsync.fetch_topics_async
+ensure
+	Faraday.default_connection.close
+end
+
