f21c3a1e642a84429fab4ea8e0d419bd73f5410c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-02-15 10:15:40 +1300

    Update copyrights.

--- examples/topics.rb
+++ examples/topics.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2020-2024, by Samuel Williams.
+# Copyright, 2020-2025, by Samuel Williams.
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
